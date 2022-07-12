d8f5a9c7f25b063bda02b57d7defdf935905d53d
- <wavevape@gmail.com>
2022-07-12 21:19:39 +0300

    Ну что же. Пришло время полной переработки приложения.
    Из того, что нужно сделать:
    1) Полностью отказаться от XML хранения данных в классе, переходим на СУБД
    2) Переработать все без исключения команды, на запросы к БД
    3) После всего сделанного разместить БД на хостинге.
    
    4) Похвастаться ребятам на работе.
    
    За работу.

--- RialDataBase_2.0/Services/DataWorker.cs
+++ RialDataBase_2.0/Services/DataWorker.cs
@@ -12,37 +12,6 @@ namespace RialDataBase_2._0.Services
 {
     public static class DataWorker
     {
-        private static readonly string path = $"{Environment.CurrentDirectory}\\Data.xml";
-
-        static XmlSerializer xmlSerializer;
-
-       public static ObservableCollection<VinWindow> LoadData()
-        {
-            
-            ObservableCollection<VinWindow> client = new ObservableCollection<VinWindow>();
-
-            using (StreamReader sr = new StreamReader(path))
-            {
-                client = (xmlSerializer.Deserialize(sr) as ObservableCollection<VinWindow>);   
-            }
-
-            return client;
-        }
-
-
-        public static void SavesData(ObservableCollection<VinWindow> Clients)
-
-        {
-            using (StreamWriter sr = new StreamWriter(path))
-            {
-                xmlSerializer.Serialize(sr, Clients);
-            }
-
-        }
-
-        static DataWorker()
-        {
-           xmlSerializer = new XmlSerializer(typeof(ObservableCollection<VinWindow>));
-        }
+        
     }
 }
